56a3545f20d856c7f2d658fc8f5c33ac2dae5690
zagorski32611 <j.j.zagorski@gmail.com>
2017-08-25 21:37:25 -0400

    successfully added flash updates

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -28,7 +28,8 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        flash[:success] = 'Todo was successfully created.'
+        format.html { redirect_to @todo }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
@@ -42,7 +43,8 @@ class TodosController < ApplicationController
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        flash[:success] = 'Todo was successfully updated.'
+        format.html { redirect_to @todo }
         format.json { render :show, status: :ok, location: @todo }
       else
         format.html { render :edit }
@@ -56,7 +58,8 @@ class TodosController < ApplicationController
   def destroy
     @todo.destroy
     respond_to do |format|
-      format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
+      flash[:danger] = 'Todo was successfully destroyed.'
+      format.html { redirect_to todos_url }
       format.json { head :no_content }
     end
   end
